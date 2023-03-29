dd35c50e88eaa2a0fe8a420562025a2784db4d04
Jayden Chapman <jaychapmancode@gmail.com>
2023-03-28 18:49:39 -0700

    Initial install and build

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// Imports react functionality
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom;"
+
+//Imports styling
+import './App.css';
+
+// Sends entire application to index.js where application is loaded and rendered
+export default function App() {
+  return (
+    <Router>
+    <div>
+      
+    </div>
+    </Router>
+  );
+};
